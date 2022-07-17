8c00a173742d99f94b2aa4b82be7096c3574b4ee
dataqao <99282330+dataqao@users.noreply.github.com>
2022-07-17 13:08:09 -0500

    Add files via upload

---
+++ analysis projectChicago Socioeconomic Indicators.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 10:20:35 2022
+
+@author: mohammad
+"""
+
+#This dataset contains a selection of six socioeconomic indicators of public health significance and a hardship index, by Chicago community area,
+#for the years 2008 – 2012.
+
+
